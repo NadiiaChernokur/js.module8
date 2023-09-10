b3c6ca101c65dd56d2734b3dd6943322392ebcc3
NadiiaChernokur <chiernokur5@gmail.com>
2023-09-10 13:50:30 +0200

    task1 task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import lodash from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+    const onPlay = function(data) {
+        localStorage.setItem("videoplayer-current-time", value)
+    };
+    
+    player.on('timeupdate', onPlay);
+
+
+    player.setCurrentTime(30.456).then(function(seconds) {
+        
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+               
+                break;
+    
+            default:
+                
+                break;
+        }
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import lodash from "lodash.throttle";
+
+const feedback = document.querySelector(".feedback-form");
+const email = feedback.querySelector("[name='email']");
+const textarea = feedback.querySelector("[name='message']")
+const submitButton = feedback.querySelector("[type='submit']");
+
+ feedback.addEventListener("input", lodash(saveMessage, 500));
+window.addEventListener("load", rebootPage);
+ submitButton.addEventListener("click", clickSubmit)
+
+function saveMessage(evt) {
+    evt.preventDefault();
+    const messegeObject = {
+        Email: email.value,
+        Message: textarea.value
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(messegeObject))
+};
+
+function rebootPage() {
+    const getItemText = localStorage.getItem("feedback-form-state")
+    const getItemTextParse = JSON.parse(getItemText);
+
+    if(getItemText) {
+        return textarea.value = getItemTextParse.Message 
+    }
+    //try {
+    //     return getItemText === null ? undefined : getItemTextParse;
+    //   } catch (error) {
+    //     console.error("Get state error: ", error.message);
+    //   }
+};
+
+function clickSubmit(e) {
+    e.preventDefault();
+    const formData = {
+        Email: email.value,
+        Message: textarea.value
+    };
+
+    console.log(formData)
+
+    email.value = "";
+    textarea.value = ""
+   
+   localStorage.removeItem("feedback-form-state");
+};
 
